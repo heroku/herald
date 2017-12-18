fe80608fb45d08735079820fdb055e1753703718
Kenneth Reitz <me@kennethreitz.org>
2017-12-18 09:21:06 -0500

    improvements to cli
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- cmd/herald-cli/main.go
+++ cmd/herald-cli/main.go
@@ -34,6 +34,14 @@ func main() {
 		},
 	}
 
+	// --is-published.
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "unpublished",
+			Usage: "Lists unpublished versions.",
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
 		// The Buildpack.
 
@@ -41,8 +49,9 @@ func main() {
 
 		bp := parsed[0]
 
+		// "python" was speficied.
 		if len(parsed) == 1 {
-			fmt.Printf("Buildpack %q targets:\n\n", bp)
+			fmt.Printf("Buildpack '%s' targets:\n\n", bp)
 
 			// Get the buildpack from Redis.
 			buildpack := herald.NewBuildpack(bp)
@@ -60,8 +69,9 @@ func main() {
 
 		target := parsed[1]
 
+		// "python:python" was speficied.
 		if len(parsed) == 2 {
-			fmt.Printf("Buildpack %q:%q targets:\n\n", bp, target)
+			fmt.Printf("Buildpack '%s:%s' releases:\n\n", bp, target)
 
 			// Get the buildpack from Redis.
 			buildpack := herald.NewBuildpack(bp)
@@ -73,7 +83,15 @@ func main() {
 
 			// Print them to the screen.
 			for _, v := range versions {
-				fmt.Printf(" - %q\n", v)
+				// if --unpublished passed, only display unpublished versions.
+				if c.Bool("unpublished") {
+					if !v.IsPublished {
+						fmt.Printf(" - %s\n", v)
+					}
+				} else {
+					fmt.Printf(" - %s\n", v)
+				}
+
 			}
 
 			os.Exit(0)
@@ -81,6 +99,7 @@ func main() {
 
 		release := parsed[2]
 
+		// "python:python:3.6.3" was speficied.
 		fmt.Printf("Info: %q:%q:%s\n", bp, target, release)
 
 		// python := herald.NewBuildpack("python")
